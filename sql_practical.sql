17d09c17b560d2c19223eea6341cfdaf5f4a1483
eng shubham kumar <engshubham66@gmail.com>
2026-09-17 17:10:04 +0530

    Add DML commands

--- sql_practical.sql
+++ sql_practical.sql
@@ -8,8 +8,10 @@
 create database Hospital_Management_System;
  use Hospital_Management_System;
  
- create table supplier(
- SID int primary key,
+ # 1. Create the tables
+drop table Supplier;
+ create table Supplier(
+ SID varchar(20) primary key,
  Sname varchar(30),
  branch varchar(30),
  city varchar(30),
@@ -17,48 +19,162 @@ create database Hospital_Management_System;
  );
  
  create table Part(
- PID int primary key,
+ PID varchar(20) primary key,
  Pname varchar(30),
  color varchar(30),
  price float
  );
- 
+
  create table Supplies(
- SID int primary key,
- PID int,
- constraint fk_supplies_part
- foreign key (PID)
- references Part(PID),
+ SID varchar(20) primary key,
+ PID varchar(20),
  qty int,
- date_supplied date
+ date_supplied date,
+ constraint fk_supplies_part
+  foreign key (PID)
+  references Part(PID) 
  );
+ 
+ # 2. Add a new attribute state in Supplier table
  
  alter table Supplier
  add state varchar(30);
  
+ # 3. Remove city from Supllier
+ 
  alter table Supplier
  drop city;
+ 
+ # 4. Modify the data type of phone attribute
  
  alter table Supplier
  modify phone varchar(30) not null;
  
+ # 5. Change the name of attribute city to address
+ 
  alter table Supplier add city varchar(30);
  alter table Supplier rename column city to address;
  
+ # 6. Change a table's name, Supplier to sup
+ 
  alter table Supplier rename to sup;
 
+# 7. Use truncate to delete the contents of Supplies table
+
 truncate table sup;
+
+# 8. Remove the Part table from database
 
 show create table Supplies;
 alter table Supplies drop foreign key fk_supplies_part;
 drop table Part;
 
-alter table sup drop foreign key PID;
-drop table Part;
+# Rename table name sup to Supplier again
 
- select * from sup;
- select * from supplies;
+Alter table sup rename to Supplier;
+# Use SELECT command
+
+ select * from Supplier;
  select * from Part;
+ select * from Supplies;
  
 /* DML commands */
+
+# 1. Insert at least 10 records in tables supplier, part and supplies  
+
+insert into Supplier
+(SID, Sname, branch, city, phone)
+ values
+('s1', 'Rohit', 'Branch A', 'Chandan Nagar', 1190283728),
+('s2', 'Sonu', 'Branch A', 'Tirkha Colony', 1190183728),
+('s3', 'Deepak', 'Branch C', 'Chandan Nagar', 1190283727),
+('s4', 'Mohit', 'Branch A', 'Panipat', 1902837286),
+('s5', 'Rohan', 'Branch A', 'Chawla Colony', 1190283725),
+('s6', 'Anuj', 'Branch B', 'Gurugram', 1190283724),
+('s7', 'Ritesh', 'Branch B', 'Chandan Nagar', 1190283723),
+('s8', 'Vishal', 'Branch C', 'Faridabad', 1190283722),
+('s9', 'Ram', 'Branch B', 'Parvtiya Colony', 1190283721),
+('s10', 'Ramesh', 'Branch C', 'Chandan Nagar', 1190283728);
+
+insert into Part
+(PID, Pname, color, price)
+values
+('p101', 'Stethoscope', 'Black', '1200'),
+('p102', 'Wheelchair', 'Blue', '8600'),
+('p103', 'Hospital Bed', 'White', '25000'),
+('p104', 'Oxygen Cylinder', 'Silver', '7000'),
+('p105', 'Blood Monitor', 'White', '3500'),
+('p106', 'Patient', 'Black', '45000'),
+('p107', 'IV Stand', 'Silver', '1800'),
+('p108', 'Examination Table', 'White', '12000'),
+('p109', 'Computer', 'Black', '45000'),
+('p1010', 'Printer', 'White', '12000');
+
+insert into Supplies
+(SID, PID, qty, date_supplied)
+values
+('s1', 'p101', 500, '2026-09-15'),
+('s2', 'p102', 500, '2026-09-15'),
+('s3', 'p103', 900, '2026-09-15'),
+('s4', 'p104', 600, '2026-09-15'),
+('s5', 'p105', 500, '2026-09-15'),
+('s6', 'p106', 500, '2026-09-15'),
+('s7', 'p107', 800, '2026-09-15'),
+('s8', 'p108', 500, '2026-09-15'),
+('s9', 'p109', 700, '2026-09-15'),
+('s10', 'p1010', 700, '2026-09-15');
  
+# 2. Show the contents in tables supplier, part and supplies  
+
+ SELECT * FROM  Supplier;
+SELECT * FROM  Part;
+SELECT * FROM Supplies;
+
+# 3. Find the name and city of all suppliers
+SELECT Sname, city FROM Supplier;
+
+# 4. Find the name and phoneno of all suppliers who stay in ‘Delhi’  
+select Sname, phone from Supplier
+where city = 'Delhi';
+
+# 5. Find all distinct branches of suppliers  
+select distinct branch from Supplier;
+
+# 6. Delete the record of the supplier whose SID is 204001  
+
+delete from Supplier 
+where SID='204001';
+
+# 7. Delete all records of supplier table 
+
+truncate table Supplier;
+
+# 8. Delete all records of suppliers whose city starts with capital A. 
+
+delete from Supplier
+where city like 'A%'
+limit 10;
+
+# 9. Find the supplier names which have ‘lk’ in any position 
+
+select * from Supplier
+where Sname like '%lk%';
+
+# 10. Find the supplier name where ‘R’ is in the second position  
+select * from Supplier
+where Sname like '-r%';
+
+# 11. Find the name of supplier whose name starts with ‘V’ and ends with ‘A’  
+select * from Supplier
+where Sname like 'v%a';
+
+# 12. Change the city of all suppliers to ‘BOMBAY’  
+set sql_safe_updates = 0;
+
+Update Supplier
+set city='BOMBAY'; 
+
+# 13. Change the city of supplier ‘Vandana’ to ‘Goa’  
+Update Supplier
+set city='Goa'
+where Sname='Vandana';
