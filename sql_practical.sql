5533ccbe129df0b4bb4b505af33d723722c5a659
eng-shubham kumar <engshubham66@gmail.com>
2026-09-22 18:15:24 +0530

    Refactor comments in sql_practical.sql for clarity

--- sql_practical.sql
+++ sql_practical.sql
@@ -1,4 +1,7 @@
- -- Hospital Management System
+--                          Pratical no. - 1
+==================================================================================================
+ 
+-- Hospital Management System
  
 /* Consider the following Schema  Supplier(SID, Sname, branch, city, phone) 
  Part(PID,Pname, color, price) 
@@ -11,8 +14,8 @@
 create database Hospital_Management_System;
  use Hospital_Management_System;
  
- # 1. Create the tables
-drop table Supplier;
+ -- 1. Create the above table 
+
  create table Supplier(
  SID varchar(20) primary key,
  Sname varchar(30),
@@ -38,45 +41,45 @@ drop table Supplier;
   references Part(PID) 
  );
  
- # 2. Add a new attribute state in Supplier table
+ -- 2. Add a new attribute state in Supplier table
  
  alter table Supplier
  add state varchar(30);
  
- # 3. Remove city from Supllier
+ -- 3. Remove city from Supllier
  
  alter table Supplier
  drop city;
  
- # 4. Modify the data type of phone attribute
+ -- 4. Modify the data type of phone attribute
  
  alter table Supplier
  modify phone varchar(30) not null;
  
- # 5. Change the name of attribute city to address
+ -- 5. Change the name of attribute city to address
  
  alter table Supplier add city varchar(30);
  alter table Supplier rename column city to address;
  
- # 6. Change a table's name, Supplier to sup
+ -- 6. Change a table's name, Supplier to sup
  
  alter table Supplier rename to sup;
 
-# 7. Use truncate to delete the contents of Supplies table
+-- 7. Use truncate to delete the contents of Supplies table
 
 truncate table sup;
 
-# 8. Remove the Part table from database
+-- 8. Remove the Part table from database
 
 show create table Supplies;
 alter table Supplies drop foreign key fk_supplies_part;
 drop table Part;
 
-# Rename table name sup to Supplier again
+-- Rename table name sup to Supplier again
 
 Alter table sup rename to Supplier;
 
-# Use SELECT command
+-- Use SELECT command
 
  select * from Supplier;
  select * from Part;
@@ -86,7 +89,7 @@ Alter table sup rename to Supplier;
 /* DML commands */
 ----------------------------------------------------------------------------------------
 
-# 1. Insert at least 10 records in tables supplier, part and supplies  
+-- 1. Insert at least 10 records in tables supplier, part and supplies  
 
 insert into Supplier
 (SID, Sname, branch, city, phone)
@@ -133,57 +136,57 @@ values
 ('s10', 'p1010', 700, '2026-09-15'),
 ('s11',	'p1011','500',	'2026-09-19');
 
-# 2. Show the contents in tables supplier, part and supplies  
+-- 2. Show the contents in tables supplier, part and supplies  
 
 SELECT * FROM  Supplier;
 SELECT * FROM  Part;
 SELECT * FROM Supplies;
 
-# 3. Find the name and city of all suppliers
+-- 3. Find the name and city of all suppliers
 SELECT Sname, city FROM Supplier;
 
-# 4. Find the name and phoneno of all suppliers who stay in ‘Delhi’  
+-- 4. Find the name and phoneno of all suppliers who stay in ‘Delhi’  
 select Sname, phone from Supplier
 where city = 'Delhi';
 
-# 5. Find all distinct branches of suppliers  
+-- 5. Find all distinct branches of suppliers  
 select distinct branch from Supplier;
 
-# 6. Delete the record of the supplier whose SID is 204001  
+-- 6. Delete the record of the supplier whose SID is 204001  
 
 delete from Supplier 
 where SID='204001';
 
-# 7. Delete all records of supplier table 
+-- 7. Delete all records of supplier table 
 
 truncate table Supplier;
 
-# 8. Delete all records of suppliers whose city starts with capital A. 
+-- 8. Delete all records of suppliers whose city starts with capital A. 
 
 delete from Supplier
 where city like 'A%'
 limit 10;
 
-# 9. Find the supplier names which have ‘lk’ in any position 
+-- 9. Find the supplier names which have ‘lk’ in any position 
 
 select * from Supplier
 where Sname like '%lk%';
 
-# 10. Find the supplier name where ‘R’ is in the second position  
+-- 10. Find the supplier name where ‘R’ is in the second position  
 select * from Supplier
 where Sname like '-r%';
 
-# 11. Find the name of supplier whose name starts with ‘V’ and ends with ‘A’  
+-- 11. Find the name of supplier whose name starts with ‘V’ and ends with ‘A’  
 select * from Supplier
 where Sname like 'v%a';
 
-# 12. Change the city of all suppliers to ‘BOMBAY’  
+-- 12. Change the city of all suppliers to ‘BOMBAY’  
 set sql_safe_updates = 0;
 
 Update Supplier
 set city='BOMBAY'; 
 
-# 13. Change the city of supplier ‘Vandana’ to ‘Goa’  
+-- 13. Change the city of supplier ‘Vandana’ to ‘Goa’  
 Update Supplier
 set city='Goa'
 where Sname='Vandana';
@@ -192,7 +195,7 @@ where Sname='Vandana';
 /* Queries with Constraints */
 ---------------------------------------------------------------------------------------
 
-# 1. Create the supplier table with Primary Key Constraint
+-- 1. Create the supplier table with Primary Key Constraint
 
 create table Supplier(
  SID varchar(20) primary key ,
@@ -202,7 +205,7 @@ create table Supplier(
  phone varchar(15)
  );
  
- # 2. Create supplies table with Foreign key Constraint 
+ -- 2. Create supplies table with Foreign key Constraint 
 
 create table Supplies(
  SID varchar(20) primary key,
@@ -214,7 +217,7 @@ create table Supplies(
   references Part(PID) 
  );
  
- # 3. Create a part table with UNIQUE Constraint 
+-- 3. Create a part table with UNIQUE Constraint 
 
  create table Part(
  PID varchar(20) primary key,
@@ -223,7 +226,7 @@ create table Supplies(
  price float
  );
  
- # 4. Create supplier Table with Check Constraints 
+ -- 4. Create supplier Table with Check Constraints 
  
  create table Supplier(
  SID varchar(20) primary key ,
@@ -233,7 +236,7 @@ create table Supplies(
  phone varchar(15)
  );
  
- # 5. Create Supplier table with Default Constraint 
+ -- 5. Create Supplier table with Default Constraint 
  
  create table Supplier(
  SID varchar(20) primary key ,
@@ -247,7 +250,7 @@ create table Supplies(
  /* Queries on TCL */
  ---------------------------------------------------------------------------------------
  
-# 1. Create Savepoints
+-- 1. Create Savepoints
 
  start transaction;
  insert into Supplier
@@ -266,7 +269,7 @@ create table Supplies(
  insert into Supplier
  values('1014', 'Sonu Kumar', 'branch c', 'Ambala', '7897324574');
  
-# 2. Rollback to SavePoints3.Use Commit to save on 
+-- 2. Rollback to SavePoints3.Use Commit to save on 
  
 rollback to savepoint sp2;
 commit;
@@ -275,7 +278,7 @@ commit;
 /* Aggregate Functions */
 --------------------------------------------------------------------------------------
 
-# 1. Find the minimum, maximum, average and sum of costs of parts
+-- 1. Find the minimum, maximum, average and sum of costs of parts
 
 select min(price) from Part;
 
@@ -283,11 +286,11 @@ select max(price) from Part;
 
 select avg(price) from Part;
 
-# 2. Count the total number of parts present
+-- 2. Count the total number of parts present
 
 select count(*) from Part;
 
-# 3. Retrieve the average cost of all parts supplied by ‘Mike’ 
+-- 3. Retrieve the average cost of all parts supplied by ‘Mike’ 
 
 select avg(p.price) 
 from Part p
@@ -296,10 +299,10 @@ join Supplier sup on s.SID = sup.SID
 where sup.Sname = 'Mike';
 
 ---------------------------------------------------------------------------------------
-# Queries on GROUP BY, HAVING AND ORDER BY Clauses 
+/* Queries on GROUP BY, HAVING AND ORDER BY Clauses */
 ---------------------------------------------------------------------------------------
 
-# 1. Display total price of parts of each color
+-- 1. Display total price of parts of each color
 
 select color, sum(price) from Part group by color;
 
@@ -309,11 +312,11 @@ which have more than 2 suppliers */
 select branch, count(*) as supplier_count from Supplier
 group by branch having count(*) > 2;
 
-# 3. Find all parts sorted by pname in ascending order and cost in descending order
+-- 3. Find all parts sorted by pname in ascending order and cost in descending order
 
 select * from Part order by Pname asc, price desc;
 
-# 4. Find the branch and the number of suppliers in that branch 
+-- 4. Find the branch and the number of suppliers in that branch 
 
 select branch, count(*) as supplier_count from Supplier
 group by branch;
